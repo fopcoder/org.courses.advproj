090965ddeb2e285c2e0a9bcf6ca4c8bc10c3cea3
fxcoder <barabass@gmail.com>
2015-08-03 22:50:16 +0300

    init

---
+++ cv-adv-proj/src/main/java/ua/kiev/prog/AdvDAO.java
@@ -0,0 +1,16 @@
+package ua.kiev.prog;
+
+import java.util.List;
+
+public interface AdvDAO {
+	List<Advertisement> list();
+
+	List<Advertisement> list( String pattern );
+
+	void add( Advertisement adv );
+
+	void delete( long id );
+
+	byte[] getPhoto( long id );
+
+}
